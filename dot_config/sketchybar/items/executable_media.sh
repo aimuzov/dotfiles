b383fc70b4021b6ec3bc7890a26b287e0bcf6b84
Aleksey Imuzov <github@aimuzov.online>
2024-01-07 02:41:21 +0400

    feat(sketchybar): move media to right & increase label padding

--- dot_config/sketchybar/items/executable_media.sh
+++ dot_config/sketchybar/items/executable_media.sh
@@ -1,15 +1,16 @@
 media=(
 	icon.background.drawing=on
 	icon.background.image=media.artwork
-	icon.background.image.corner_radius=9
+	icon.background.image.corner_radius=14
 	icon.background.image.scale=0.9
 	script="$PLUGIN_DIR/media.sh"
 	click_script="open -a 'Яндекс Музыка'"
-	label.max_chars=30
+	label.max_chars=50
+	label.padding_left=12
 	scroll_texts=on
 	updates=on
 )
 
-sketchybar --add item media center \
+sketchybar --add item media right \
 	--set media "${media[@]}" \
 	--subscribe media media_change
