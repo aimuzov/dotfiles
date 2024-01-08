33bfce29fcd45f9c784224e41e11359fa987ed66
Aleksey Imuzov <github@aimuzov.online>
2024-01-08 15:58:15 +0400

    style(install-script): reformat

--- dot_config/executable_dot_install.sh
+++ dot_config/executable_dot_install.sh
@@ -18,21 +18,21 @@ brew bundle install --global
 
 echo "Changing macOS defaults..."
 
-defaults write com.apple.LaunchServices "LSQuarantine"		-bool "false"
+defaults write com.apple.LaunchServices "LSQuarantine" -bool "false"
 
-defaults write com.apple.dock			"autohide"			-bool "true"
-defaults write com.apple.dock			"mru-spaces"		-bool "false"
-defaults write com.apple.spaces			"spans-displays"	-bool "false"
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock "mru-spaces" -bool "false"
+defaults write com.apple.spaces "spans-displays" -bool "false"
 
-defaults write com.apple.screencapture	"location"			-string "$HOME/temp/screenshots"
-defaults write com.apple.screencapture	"disable-shadow"	-bool true
-defaults write com.apple.screencapture	"type"				-string "png"
+defaults write com.apple.screencapture "location" -string "$HOME/temp/screenshots"
+defaults write com.apple.screencapture "disable-shadow" -bool true
+defaults write com.apple.screencapture "type" -string "png"
 
-defaults write com.apple.finder			"DisableAllAnimations"				-bool "true"
-defaults write com.apple.finder			"ShowExternalHardDrivesOnDesktop"	-bool "false"
-defaults write com.apple.finder			"ShowHardDrivesOnDesktop"			-bool "false"
-defaults write com.apple.finder			"ShowMountedServersOnDesktop"		-bool "false"
-defaults write com.apple.finder			"ShowRemovableMediaOnDesktop"		-bool "false"
-defaults write com.apple.Finder			"AppleShowAllFiles"					-bool "true"
+defaults write com.apple.finder "DisableAllAnimations" -bool "true"
+defaults write com.apple.finder "ShowExternalHardDrivesOnDesktop" -bool "false"
+defaults write com.apple.finder "ShowHardDrivesOnDesktop" -bool "false"
+defaults write com.apple.finder "ShowMountedServersOnDesktop" -bool "false"
+defaults write com.apple.finder "ShowRemovableMediaOnDesktop" -bool "false"
+defaults write com.apple.Finder "AppleShowAllFiles" -bool "true"
 
 killall Dock
