9a7832ff63359ad22daabbcc329e3ca55292a971
Aleksey Imuzov <github@aimuzov.online>
2024-01-09 21:08:26 +0400

    feat(raycast): add script for restart vm

---
+++ dot_bin/raycast/executable_restart-vm.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title restart vm
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon https://github.com/koekeishiya/yabai/blob/master/assets/icon/2x/icon-32px@2x.png?raw=true
+
+restart-vm
