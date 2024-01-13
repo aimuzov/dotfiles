699870a5d7b0777f8bc89446695313e2fc80a0a0
Aleksey Imuzov <github@aimuzov.online>
2024-01-14 00:13:02 +0400

    feat(raycast): add dota to script command

---
+++ dot_bin/raycast/executable_dota2.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Dota 2
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_symbol.png
+
+sh "/Users/aimuzov/Library/Application Support/Steam/steamapps/common/dota 2 beta/game/dota.sh"
