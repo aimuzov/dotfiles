ae4d88a14982779d7e24760c9f67b026ceb272b7
Aleksey Imuzov <github@aimuzov.online>
2024-02-07 19:11:47 +0400

    fix(nvim): fix incorrect keymap (openingh)

--- dot_config/nvim/lua/plugins/coding/git.lua
+++ dot_config/nvim/lua/plugins/coding/git.lua
@@ -29,8 +29,8 @@ return {
 		"Almo7aya/openingh.nvim",
 		optional = true,
 		keys = {
-			{ "<leader>go", "<cmd>OpenInGHFile+<cr>", desc = "Open in github/gitlab", mode = { "n" } },
-			{ "<leader>go", "<esc><cmd>'<,'>OpenInGHFileLines+<cr>", desc = "Open in github/gitlab", mode = { "v" } },
+			{ "<leader>go", "<cmd>OpenInGHFile<cr>", desc = "Open in github/gitlab", mode = { "n" } },
+			{ "<leader>go", "<esc><cmd>'<,'>OpenInGHFileLines<cr>", desc = "Open in github/gitlab", mode = { "v" } },
 		},
 	},
 
