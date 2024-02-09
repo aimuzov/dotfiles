12d37be0958780e89ab285336422f578d6b1b558
Aleksey Imuzov <github@aimuzov.online>
2024-02-09 17:31:08 +0400

    fix(nvim): update build script for js debug dep

--- dot_config/nvim/lua/plugins/debugging.lua
+++ dot_config/nvim/lua/plugins/debugging.lua
@@ -7,7 +7,7 @@ return {
 			{
 				"microsoft/vscode-js-debug",
 				version = "1.x",
-				build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+				build = "npm install && npx gulp vsDebugServerBundle && mv dist out",
 			},
 		},
 		config = function()
