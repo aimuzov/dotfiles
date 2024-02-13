b9db4c5a776a0f7ceda1ff55e6a25054f8d58481
Aleksey Imuzov <github@aimuzov.online>
2024-02-13 10:22:54 +0400

    fix(nvim): fix incorrect passing args (get highlight by name)

--- dot_config/nvim/lua/util/hi.lua
+++ dot_config/nvim/lua/util/hi.lua
@@ -1,7 +1,7 @@
 local util_other = require("util.other")
 
 local function update_hl(group, value)
-	local is_ok, hl_def = pcall(vim.api.nvim_get_hl, group, { create = true })
+	local is_ok, hl_def = pcall(vim.api.nvim_get_hl, 0, { name = group, create = true })
 
 	if hl_def[true] or not is_ok then
 		hl_def = value
