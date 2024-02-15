ccc1d0a3b977688379d074e1613383fcbfc34bdd
Aleksey Imuzov <github@aimuzov.online>
2024-02-15 10:10:58 +0400

    fix(nvim): disable kaiten plugin

--- dot_config/nvim/lua/plugins/coding/trackers.lua
+++ dot_config/nvim/lua/plugins/coding/trackers.lua
@@ -2,7 +2,7 @@ return {
 	{
 		dir = "~/projects/aimuzov/kaiten.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
-		enabled = true,
+		enabled = false,
 		config = true,
 		opts = {},
 	},
