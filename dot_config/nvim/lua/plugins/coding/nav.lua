81ed5e3d8d350cb2cf514481aef4eb15bcb59bb8
Aleksey Imuzov <github@aimuzov.online>
2024-02-20 09:01:55 +0400

    fix(nvim): disable ts context for svelte (exception fix)

--- dot_config/nvim/lua/plugins/coding/nav.lua
+++ dot_config/nvim/lua/plugins/coding/nav.lua
@@ -42,7 +42,20 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		optional = true,
-		keys = { { "[x", [[<cmd>lua require("treesitter-context").go_to_context()<cr>]] } },
+		opts = {
+			on_attach = function(buf)
+				local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
+
+				if ft == "" or ft == "svelte" then
+					return false
+				end
+
+				return true
+			end,
+		},
+		keys = {
+			{ "[x", [[<cmd>lua require("treesitter-context").go_to_context()<cr>]] },
+		},
 	},
 
 	{
--- dot_config/nvim/lua/plugins/formatting.lua
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -1,12 +1,14 @@
+local prettier_list = { "prettierd", "prettier" }
+
 return {
 	{
 		"stevearc/conform.nvim",
 		optional = true,
 		opts = {
 			formatters_by_ft = {
-				["svelte"] = { "stylelint", "prettier" },
-				["css"] = { "stylelint", "prettier" },
-				["typescript"] = { "prettier", "lsp" },
+				["svelte"] = { prettier_list },
+				["css"] = { prettier_list, "stylelint" },
+				["typescript"] = { prettier_list, "lsp" },
 				["zsh"] = { "shfmt" },
 			},
 		},
