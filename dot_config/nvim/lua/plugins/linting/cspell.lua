9c9f165a7efc4dff359be874210b8d75f4359fad
Aleksey Imuzov <github@aimuzov.online>
2024-02-20 09:06:08 +0400

    feat(nvim): replace cspell plugin by nvim-lint

--- dot_config/nvim/lua/plugins/linting/cspell.lua
+++ dot_config/nvim/lua/plugins/linting/cspell.lua
@@ -1,17 +1,14 @@
 return {
 	{
-		"nvimtools/none-ls.nvim",
-		dependencies = { "davidmh/cspell.nvim" },
+		"mfussenegger/nvim-lint",
 		optional = true,
 
 		opts = function(_, opts)
 			if vim.fn.executable("cspell") == 1 then
-				local cspell = require("cspell")
-				local cspell_config = { config_file_preferred_name = "cspell.config.json" }
-
-				table.insert(opts.sources, cspell.diagnostics.with({ config = cspell_config }))
-				table.insert(opts.sources, cspell.code_actions.with({ config = cspell_config }))
+				opts.linters_by_ft["*"] = { "cspell" }
 			end
+
+			return opts
 		end,
 	},
 }
