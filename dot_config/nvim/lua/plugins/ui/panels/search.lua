6dec4ce6bce2dc6ec75b09645f07627131bb1263
Aleksey Imuzov <github@aimuzov.online>
2024-02-20 12:01:53 +0400

    style(nvim): prettify spectre config

--- dot_config/nvim/lua/plugins/ui/panels/search.lua
+++ dot_config/nvim/lua/plugins/ui/panels/search.lua
@@ -3,12 +3,13 @@ return {
 		"nvim-pack/nvim-spectre",
 		optional = true,
 		opts = {
-			line_sep_start = "┌──────────────────────────────────────────────────────────────────────────",
-			result_padding = "│   ",
-			line_sep = "└──────────────────────────────────────────────────────────────────────────",
-			highlight = {
-				border = "SpectreBorderCustom",
-			},
+			highlight = { border = "SpectreBorderCustom" },
+
+			-- stylua: ignore start
+			line_sep_start =	"┌──────────────────────────────────────────────────────────────────────────",
+			result_padding =	"│   ",
+			line_sep =			"└──────────────────────────────────────────────────────────────────────────",
+			-- stylua: ignore end
 		},
 	},
 }
