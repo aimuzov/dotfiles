309752cd962828e68f1a9995fda27522c53a9058
Aleksey Imuzov <github@aimuzov.online>
2024-02-27 22:32:51 +0400

    fix(sketchybar): fix colors

--- dot_config/sketchybar/config.lua
+++ dot_config/sketchybar/config.lua
@@ -4,14 +4,14 @@ return {
 
 	colors = {
 		black = 0xff181926,
-		white = 0xffffffff,
+		white = 0xeeffffff,
 		red = 0xffed8796,
 		green = 0xffa6da95,
 		blue = 0xff8aadf4,
 		yellow = 0xffeed49f,
 		orange = 0xfff5a97f,
 		magenta = 0xffc6a0f6,
-		grey = 0xff939ab7,
+		grey = 0xddc6d0f5,
 		transparent = 0x00000000,
 
 		bar = {
