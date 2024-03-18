6943789e61b80fba97b7e724edced7bae632c74d
Aleksey Imuzov <github@aimuzov.online>
2024-03-18 17:34:09 +0400

    fix(nvim): migrate to v2 (rest.nvim)

--- dot_config/nvim/lua/plugins/ui/panels/rest.lua
+++ dot_config/nvim/lua/plugins/ui/panels/rest.lua
@@ -1,10 +1,19 @@
 return {
 	{
+		"vhyrro/luarocks.nvim",
+		optional = true,
+		config = true,
+	},
+	{
 		"rest-nvim/rest.nvim",
-		dependencies = { { "nvim-lua/plenary.nvim" } },
 		main = "rest-nvim",
 		optional = true,
 		config = true,
+		ft = "http",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"vhyrro/luarocks.nvim",
+		},
 		keys = {
 			{ "<cr>", [[<cmd>lua require("rest-nvim").run()<cr>]], ft = "http" },
 		},
