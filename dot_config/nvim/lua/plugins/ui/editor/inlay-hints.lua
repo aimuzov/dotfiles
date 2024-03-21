0a2a19a834afffd230e6e32120e11172852bff55
Aleksey Imuzov <github@aimuzov.online>
2024-03-21 12:51:46 +0400

    fix(nvim): increate delay for gitblame inlay hint (gitsigns)

--- dot_config/nvim/lua/plugins/ui/editor/inlay-hints.lua
+++ dot_config/nvim/lua/plugins/ui/editor/inlay-hints.lua
@@ -34,7 +34,7 @@ return {
 		optional = true,
 		opts = {
 			current_line_blame = true,
-			current_line_blame_opts = { delay = 50 },
+			current_line_blame_opts = { delay = 250 },
 			current_line_blame_formatter = " 󰞗 <author>  <author_time:%R>  <summary>",
 		},
 	},
