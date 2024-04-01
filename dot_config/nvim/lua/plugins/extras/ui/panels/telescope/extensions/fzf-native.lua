0b184eb136928707ba5e02431ed237853e7eb561
Aleksey Imuzov <github@aimuzov.online>
2024-04-01 15:08:29 +0400

    fix(nvim): remove config func for fzf-native extension (telescope)

--- dot_config/nvim/lua/plugins/extras/ui/panels/telescope/extensions/fzf-native.lua
+++ dot_config/nvim/lua/plugins/extras/ui/panels/telescope/extensions/fzf-native.lua
@@ -1,8 +1,4 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
-
-	config = function()
-		require("telescope").load_extension("fzf")
-	end,
 }
