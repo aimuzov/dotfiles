b3089d18ef71c3de42ca0f4832e4882aef1310a2
Aleksey Imuzov <github@aimuzov.online>
2024-04-01 15:24:54 +0400

    fix(nvim): replace githublinks plugin to fugitive provider for github

---
+++ dot_config/nvim/lua/plugins/extras/git/fugitive/base.lua
@@ -0,0 +1,8 @@
+return {
+	"tpope/vim-fugitive",
+
+	keys = {
+		{ "<leader>go", "<cmd>GBrowse<cr>", desc = "Open in browser", mode = { "n" } },
+		{ "<leader>go", "<cmd>'<,'>GBrowse<cr>", desc = "Open in browser", mode = { "v" } },
+	},
+}
---
+++ dot_config/nvim/lua/plugins/extras/git/fugitive/providers/github.lua
@@ -0,0 +1,4 @@
+return {
+	"tpope/vim-rhubarb",
+	dependencies = { "tpope/vim-fugitive" },
+}
