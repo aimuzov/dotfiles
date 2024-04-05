4dc296041fe4b47a03759b80a8e6346f7fba34a5
Aleksey Imuzov <github@aimuzov.online>
2024-04-05 13:54:30 +0400

    fix(nvim): remove status column plugin

--- dot_config/nvim/lua/plugins/extras/ui/editor/status-column.lua
+++ dot_config/nvim/lua/plugins/extras/ui/editor/status-column.lua
@@ -3,27 +3,4 @@ return {
 		"lewis6991/gitsigns.nvim",
 		opts = { numhl = true },
 	},
-
-	{
-		"luukvbaal/statuscol.nvim",
-		branch = "0.10",
-		opts = function(_, opts)
-			local builtin = require("statuscol.builtin")
-
-			opts.relculright = true
-			opts.segments = {
-				{ text = { "%C" }, click = "v:lua.ScFa" },
-				{
-					text = { builtin.lnumfunc, " " },
-					condition = { true, builtin.not_empty },
-					click = "v:lua.ScLa",
-				},
-				{
-					sign = { namespace = { "diagnostic" } },
-					click = "v:lua.ScSa",
-					text = { "%s" },
-				},
-			}
-		end,
-	},
 }
