490b443427c1592e70d0b45702b4623bfb6d0ab3
Aleksey Imuzov <github@aimuzov.online>
2024-04-05 13:53:32 +0400

    fix(nvim): remove hack (diagnostic sign icon)

--- dot_config/nvim/lua/plugins/extras/hacks/lazyvim-fix-diagnostic-sign-define.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"LazyVim/LazyVim",
-	opts = function()
-		for severity, icon in pairs(LazyVim.config.icons.diagnostics) do
-			local name = "DiagnosticSign" .. severity
-			vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
-		end
-	end,
-}
