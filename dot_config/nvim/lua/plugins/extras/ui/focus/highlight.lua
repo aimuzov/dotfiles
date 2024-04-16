8099c08a37cc3cf05f544148937d0100e6ff5834
Aleksey Imuzov <github@aimuzov.online>
2024-04-16 07:49:29 +0400

    feat(nvim): disable highlight for visual modes

--- dot_config/nvim/lua/plugins/extras/ui/focus/highlight.lua
+++ dot_config/nvim/lua/plugins/extras/ui/focus/highlight.lua
@@ -5,6 +5,7 @@ return {
 			delay = 250,
 			providers = { "lsp", "treesitter", "regex" },
 			filetypes_denylist = { "alpha", "aerial", "markdown", "neo-tree", "lazyterm", "DiffviewFiles", "Glance" },
+			modes_denylist = { "v", "V" },
 			min_count_to_highlight = 2,
 		},
 	},
