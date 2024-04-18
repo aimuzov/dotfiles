212397489f34319b43f4ffa86d43214a4c6d4325
Aleksey Imuzov <github@aimuzov.online>
2024-04-18 13:40:06 +0400

    fix(nvim): fix exception (nil value)

--- dot_config/nvim/lua/plugins/extras/ui/style/panels/sidebar-pretty-offset.lua
+++ dot_config/nvim/lua/plugins/extras/ui/style/panels/sidebar-pretty-offset.lua
@@ -13,15 +13,18 @@ return {
 				if Offset.edgy then
 					Offset.get = function()
 						local layout = require("edgy.config").layout
-						local left_width = #layout.left.wins > 0 and layout.left.bounds.width or 0
-						local right_width = #layout.right.wins > 0 and layout.right.bounds.width or 0
+						local width_left = (layout.left ~= nil and #layout.left.wins > 0) and layout.left.bounds.width
+							or 0
+						local width_right = (layout.right ~= nil and #layout.right.wins > 0)
+								and layout.right.bounds.width
+							or 0
 
 						local ret = {
-							left = "%#EdgyTitle#" .. string.rep(" ", left_width) .. "%*",
-							right = "%#EdgyTitle#" .. string.rep(" ", right_width) .. "%*",
-							left_size = left_width,
-							right_size = right_width,
-							total_size = left_width + right_width,
+							left = "%#EdgyTitle#" .. string.rep(" ", width_left) .. "%*",
+							right = "%#EdgyTitle#" .. string.rep(" ", width_right) .. "%*",
+							left_size = width_left,
+							right_size = width_right,
+							total_size = width_left + width_right,
 						}
 
 						return ret
