04e0448ce057188b16e86159d040155236e7cd7a
Aleksey Imuzov <github@aimuzov.online>
2024-04-22 11:02:04 +0400

    fix(nvim): fix lint config (cspell)

--- dot_config/nvim/lua/plugins/extras/linting/cspell.lua
+++ dot_config/nvim/lua/plugins/extras/linting/cspell.lua
@@ -3,11 +3,12 @@ return {
 		"mfussenegger/nvim-lint",
 
 		opts = function(_, opts)
-			if vim.fn.executable("cspell") == 1 then
-				opts.linters_by_ft["*"] = { "cspell" }
+			if vim.fn.executable("cspell") == 0 then
+				return
 			end
 
-			return opts
+			opts.linters_by_ft["*"] = opts.linters_by_ft["*"] or {}
+			table.insert(opts.linters_by_ft["*"], "cspell")
 		end,
 	},
 }
