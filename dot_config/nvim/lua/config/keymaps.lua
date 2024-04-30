db302a5b81a3883c7d5eb33b43d55ee1173f91a0
Aleksey Imuzov <github@aimuzov.online>
2024-04-30 15:54:34 +0400

    feat(nvim): add keymap for sort lines

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,6 +7,8 @@ vim.keymap.set({ "i" }, "<c-s-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up"
 vim.keymap.set({ "v" }, "<c-s-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 vim.keymap.set({ "v" }, "<c-s-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 
+vim.keymap.set({ "v" }, "<c-i>", "<cmd>'<,'>sort i<cr>", { desc = "Sort lines" })
+
 vim.keymap.set("n", "<leader>w\\", "<C-W>v", { desc = "Split window right" })
 vim.keymap.set("n", "<leader>\\", "<C-W>v", { desc = "Split window right" })
 
