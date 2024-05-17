4887c4cef1c65260a076f880948035950a646df8
Aleksey Imuzov <github@aimuzov.online>
2024-05-17 12:16:59 +0400

    fix(nvim): improve svelte config

--- dot_config/nvim/lua/plugins/extras/lang/svelte.lua
+++ dot_config/nvim/lua/plugins/extras/lang/svelte.lua
@@ -1,31 +1,36 @@
+local function get_capabilities()
+	local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+	lsp_capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
+	lsp_capabilities.textDocument.completion = capabilities.textDocument.completion
+end
+
+local function notify_ts_files_changed()
+	LazyVim.lsp.on_attach(function(client)
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			pattern = { "*.js", "*.ts" },
+			callback = function(ctx)
+				client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+			end,
+		})
+	end)
+end
+
 return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = { "hrsh7th/cmp-nvim-lsp" },
 		opts = {
-			servers = {
-				svelte = {
-					on_attach = function(client)
-						vim.api.nvim_create_autocmd("BufWritePost", {
-							pattern = { "*.js", "*.ts" },
-							callback = function(ctx)
-								client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-							end,
-						})
-					end,
-				},
-			},
+			servers = { svelte = {} },
 
 			setup = {
 				svelte = function(_, opts)
 					local lspconfig = require("lspconfig")
-					local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
-					local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-					lsp_capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
-					lsp_capabilities.textDocument.completion = capabilities.textDocument.completion
+					opts.capabilities = get_capabilities()
 
-					opts.capabilities = capabilities
+					notify_ts_files_changed()
 					lspconfig.svelte.setup(opts)
 				end,
 			},
@@ -36,7 +41,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		optional = true,
 		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { "svelte", "scss" })
+			vim.list_extend(opts.ensure_installed, { "svelte" })
 		end,
 	},
 }
