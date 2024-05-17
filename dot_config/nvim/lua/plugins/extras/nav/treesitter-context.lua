9477f1a6eb75c55ca8f4c0d6393953cbd818d314
Aleksey Imuzov <github@aimuzov.online>
2024-05-17 11:47:32 +0400

    fix(nvim): fix keymap for ts-context

--- dot_config/nvim/lua/plugins/extras/nav/treesitter-context.lua
+++ dot_config/nvim/lua/plugins/extras/nav/treesitter-context.lua
@@ -2,6 +2,6 @@ return {
 	"nvim-treesitter/nvim-treesitter-context",
 	optional = true,
 	keys = {
-		{ "[c", [[<cmd>lua require("treesitter-context").go_to_context()<cr>]] },
+		{ "[x", [[<cmd>lua require("treesitter-context").go_to_context(vim.v.count1)<cr>]], desc = "Go to context" },
 	},
 }
