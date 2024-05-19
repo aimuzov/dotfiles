6bc7adb57153efa213dcde70a685453648bda266
Aleksey Imuzov <github@aimuzov.online>
2024-05-19 09:26:55 +0400

    fix(nvim): fix syntax for postcss (ft=svelte)

--- dot_config/nvim/lua/plugins/extras/lang/svelte.lua
+++ dot_config/nvim/lua/plugins/extras/lang/svelte.lua
@@ -42,7 +42,7 @@ return {
 		optional = true,
 		opts = function(_, opts)
 			opts.ensure_installed = opts.ensure_installed or {}
-			vim.list_extend(opts.ensure_installed, { "svelte" })
+			vim.list_extend(opts.ensure_installed, { "svelte", "scss" })
 		end,
 	},
 }
