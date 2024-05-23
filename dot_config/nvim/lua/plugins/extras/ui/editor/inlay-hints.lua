803117515e8658794882275a62c384c16f197c70
Aleksey Imuzov <github@aimuzov.online>
2024-05-23 15:54:17 +0400

    fix(nvim): enable inlay hint by default

--- dot_config/nvim/lua/plugins/extras/ui/editor/inlay-hints.lua
+++ dot_config/nvim/lua/plugins/extras/ui/editor/inlay-hints.lua
@@ -2,7 +2,6 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		opts = {
-			inlay_hints = { enabled = false },
 			diagnostics = {
 				underline = false,
 				virtual_text = { spacing = 0, prefix = "▎", suffix = " " },
