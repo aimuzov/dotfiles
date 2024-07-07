73f82107328429e44912b1a5fa3a32d578a4f9b6
Aleksey Imuzov <github@aimuzov.online>
2024-07-08 01:51:24 +0400

    fix(sketchybar): replace asdf by mise for nvim item

--- dot_config/sketchybar/items/right/nvim.lua
+++ dot_config/sketchybar/items/right/nvim.lua
@@ -22,7 +22,7 @@ local function update()
 	-- stylua: ignore start
 	local version_next_file = assert(io.popen([[gh api --method GET /repos/neovim/neovim/releases | jq -r 'map(select(.tag_name == "]]..tag..[["))[0] | .body | capture("```\n(?<version>NVIM(.*))\n```\n\n"; "m") | .version']]))
 	local version_next_result = assert(version_next_file:read("a"))
-	local version_current_file = assert(io.popen([[$(asdf where neovim ]]..tag..[[)/bin/nvim --version | sed -e '4d']]))
+	local version_current_file = assert(io.popen([[$(mise where neovim ]]..tag..[[)/bin/nvim --version | sed -e '4d']]))
 	local version_current_result = assert(version_current_file:read("a"))
 	-- stylua: ignore end
 
