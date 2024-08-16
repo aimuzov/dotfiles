8f0389e4e973bd10a389f1fe660db27f07cb6d6a
Aleksey Imuzov <github@aimuzov.online>
2024-08-16 21:46:32 +0400

    feat(zsh): add vi-mode plugin

--- dot_config/zsh/plugins.zsh
+++ dot_config/zsh/plugins.zsh
@@ -8,9 +8,15 @@ plugins=(
 	zsh-autosuggestions
 	zsh-interactive-cd
 	zsh-syntax-highlighting
+	zsh-vi-mode
 )
 
 ## ---------------------------------------------------------------------------------------------------------------------
 
 THEME_NAME=$([[ $MACOS_IS_DARK == "yes" ]] && echo "macchiato" || echo "latte")
 source "$ZSH/custom/plugins/zsh-syntax-highlighting/themes/$THEME_NAME.zsh"
+
+## ---------------------------------------------------------------------------------------------------------------------
+
+ZVM_VI_HIGHLIGHT_FOREGROUND=black
+ZVM_VI_HIGHLIGHT_BACKGROUND=magenta
