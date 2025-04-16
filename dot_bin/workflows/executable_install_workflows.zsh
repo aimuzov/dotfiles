aee2934a1f841d1e896274d9e36c117c88fc9207
Aleksey Imuzov <github@aimuzov.online>
2025-04-16 09:53:16 +0400

    fix(bin): fix workflows setup script

--- dot_bin/workflows/executable_install_workflows.zsh
+++ dot_bin/workflows/executable_install_workflows.zsh
@@ -15,8 +15,13 @@ for filename in ./workflows_temp/*.workflow; do
 	open $filename
 	sleep 1
 	osascript -e 'tell application "System Events" to click button "Install" of window "Quick Action Installer" of process "Automator Installer"'
-	sleep 1
-	osascript -e 'tell application "System Events" to click button "Done" of window "Quick Action Installer" of process "Automator Installer"'
+	{
+		sleep 1
+		osascript -e 'tell application "System Events" to click button "Done" of window "Quick Action Installer" of process "Automator Installer"'
+	} || {
+		sleep 1
+		osascript -e 'tell application "System Events" to click button "Replace" of window "Quick Action Installer" of process "Automator Installer"'
+	}
 done
 
 rm -rf ./workflows_temp
