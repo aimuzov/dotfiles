a7b8269ed4ebd9f6325612d922e256e166b57de5
Aleksey Imuzov <github@aimuzov.online>
2025-08-27 12:09:49 +0400

    fix(fish): fix disabling nvim builtins colorschemes

--- dot_config/fish/functions/core/nvim_disable_builtin_colorschemes.fish
+++ dot_config/fish/functions/core/nvim_disable_builtin_colorschemes.fish
@@ -1,23 +1,20 @@
 # https://github.com/neovim/neovim/issues/15083#issuecomment-1987041311
 
 function nvim_disable_builtin_colorschemes
-    set -l base_path "$HOME/.local/share/mise/installs/neovim"
+    set -l base_path (mise where neovim)
+    set -l colors_path "$base_path/share/nvim/runtime/colors"
 
-    for dir in (ls $base_path)
-        set -l colors_path "$base_path/$dir/nvim-macos-arm64/share/nvim/runtime/colors"
+    if test -d $colors_path
+        set -l files $colors_path/*.vim
 
-        if test -d $colors_path
-            set -l files $colors_path/*.vim
-
-            for file in $files
-                if test "$file" != "default.vim"
-                    mv "$file" "$file.disabled"
-                end
+        for file in $files
+            if test "$file" != "default.vim"
+                mv "$file" "$file.disabled"
             end
+        end
 
-            if test -e "$colors_path/vim.lua"
-                mv "$colors_path/vim.lua" "$colors_path/vim.lua.disabled"
-            end
+        if test -e "$colors_path/vim.lua"
+            mv "$colors_path/vim.lua" "$colors_path/vim.lua.disabled"
         end
     end
 end
