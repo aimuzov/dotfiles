2b0dab5d8e4845406b4a17062d7f97b9b6b465b6
Aleksey Imuzov <github@aimuzov.online>
2025-11-25 15:27:01 +0400

    fix(fish): update git current branch fn

--- dot_config/fish/functions/core/git/__git.current_branch.fish
+++ dot_config/fish/functions/core/git/__git.current_branch.fish
@@ -1,5 +1,3 @@
 function __git.current_branch -d "Output git's current branch name"
-    begin
-        git symbolic-ref HEAD; or git rev-parse --short HEAD; or return
-    end 2>/dev/null | sed -e 's|^refs/heads/||'
+    git branch --show-current 2>/dev/null; or git rev-parse --short HEAD 2>/dev/null; or return
 end
