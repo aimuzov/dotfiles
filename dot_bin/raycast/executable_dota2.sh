f11de4cb4813043b965018613d2d618480d54dde
Aleksey Imuzov <github@aimuzov.online>
2026-01-19 16:49:40 +0400

    chore(raycast): unify script metadata and fix title typo

--- dot_bin/raycast/executable_dota2.sh
+++ dot_bin/raycast/executable_dota2.sh
@@ -4,6 +4,7 @@
 # @raycast.schemaVersion 1
 # @raycast.title Dota 2
 # @raycast.mode silent
+# @raycast.packageName 􀩼
 
 # Optional parameters:
 # @raycast.icon https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/global/dota2_logo_symbol.png
--- dot_bin/raycast/executable_restart-vm.sh
+++ dot_bin/raycast/executable_restart-vm.sh
@@ -2,8 +2,9 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title restart vm
+# @raycast.title Restart WM
 # @raycast.mode silent
+# @raycast.packageName 􀩼
 
 # Optional parameters:
 # @raycast.icon https://github.com/koekeishiya/yabai/blob/master/assets/icon/2x/icon-32px@2x.png?raw=true
