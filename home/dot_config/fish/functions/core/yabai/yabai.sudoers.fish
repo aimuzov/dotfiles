24d13a5c350a8bb43a678eaf76b11a58627e31bf
Aleksey Imuzov <github@aimuzov.online>
2026-02-16 09:54:46 +0400

    fix(fish): fix yabai sudoers entry generation
    Use whoami instead of hardcoded username, extract only hash with cut,
    and include full yabai path in sudoers format.

--- home/dot_config/fish/functions/core/yabai/yabai.sudoers.fish
+++ home/dot_config/fish/functions/core/yabai/yabai.sudoers.fish
@@ -1,5 +1,5 @@
 function yabai.sudoers
     # https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
-    echo "aimuzov ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai)) --load-sa" | pbcopy
+    echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | pbcopy
     sudo visudo -f /private/etc/sudoers.d/yabai
 end
