bf3e2da9dc6f4b13e80c76f5a1fe3587477393b6
Aleksey Imuzov <github@aimuzov.online>
2026-02-23 15:15:34 +0400

    fix(yabai): skip --display if space is already on target display

--- home/dot_config/fish/functions/core/yabai/yabai.space_create.fish
+++ home/dot_config/fish/functions/core/yabai/yabai.space_create.fish
@@ -4,13 +4,19 @@ function yabai.space_create
     set -l name $argv[3]
     # $argv[4] is "--" separator
     set -l apps $argv[5..-1]
-    set -l space (yabai -m query --spaces | jq "first(.[] | select(.index == $idx) | .index)")
+    set -l spaces_json (yabai -m query --spaces)
+    set -l space (echo $spaces_json | jq "first(.[] | select(.index == $idx) | .index)")
+    set -l current_display (echo $spaces_json | jq "first(.[] | select(.index == $idx) | .display)")
 
     if test -z "$space"
         yabai -m space --create
     end
 
-    yabai -m space "$idx" --label "$name" --display "$display"
+    yabai -m space "$idx" --label "$name"
+
+    if test "$current_display" != "$display"
+        yabai -m space "$idx" --display "$display"
+    end
 
     if test (count $apps) -gt 0
         set -l pattern "^("(string join "|" $apps)")\$"
