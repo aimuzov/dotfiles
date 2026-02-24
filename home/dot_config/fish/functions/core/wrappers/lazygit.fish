ba98acbf453a618965be6155d071746a22cbecf0
Aleksey Imuzov <github@aimuzov.online>
2026-02-24 10:04:00 +0400

    feat(lazygit): change directory to last visited on exit

--- home/dot_config/fish/functions/core/wrappers/lazygit.fish
+++ home/dot_config/fish/functions/core/wrappers/lazygit.fish
@@ -1,3 +1,9 @@
 function lazygit --description 'use lazygit with specific theme file' --wraps lazygit
+    set -gx LAZYGIT_NEW_DIR_FILE "$XDG_CONFIG_HOME/lazygit/newdir"
     command lazygit --use-config-file="$XDG_CONFIG_HOME/lazygit/config.yml,$XDG_CONFIG_HOME/lazygit/theme-catppuccin-$CATPPUCCIN_FLAVOR.yml" $argv
+
+    if test -f $LAZYGIT_NEW_DIR_FILE
+        cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+        rm -f $LAZYGIT_NEW_DIR_FILE >/dev/null
+    end
 end
