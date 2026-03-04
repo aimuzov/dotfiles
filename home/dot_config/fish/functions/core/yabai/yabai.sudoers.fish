4c756de12c1ca0b9333bdd9db44f5cb6f1e29382
Aleksey Imuzov <github@aimuzov.online>
2026-03-04 09:34:18 +0400

    fix(yabai): rewrite sudoers helper to apply rules directly via tee

--- home/dot_config/fish/functions/core/yabai/yabai.sudoers.fish
+++ home/dot_config/fish/functions/core/yabai/yabai.sudoers.fish
@@ -1,5 +1,9 @@
 function yabai.sudoers
     # https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
-    echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | pbcopy
-    sudo visudo -f /private/etc/sudoers.d/yabai
+
+    set yabai_path (which yabai)
+    set yabai_hash (shasum -a 256 $yabai_path | cut -d " " -f 1)
+    set sudoers_line (whoami)" ALL=(root) NOPASSWD: sha256:$yabai_hash $yabai_path --load-sa"
+
+    echo $sudoers_line | sudo tee /private/etc/sudoers.d/yabai >/dev/null
 end
